af8b00e6c6fecc15694f4e18cd58650ec7d43f3c
Elias Mueggler <elias.mueggler@gmail.com>
2014-04-01 10:43:40 +0200

    new rendering method

--- dvs_renderer/src/renderer.cpp
+++ dvs_renderer/src/renderer.cpp
@@ -12,25 +12,61 @@
 
 image_transport::Publisher image_pub_;
 
-void eventsCallback(const dvs_msgs::EventArray::ConstPtr& msg) {
-  cv_bridge::CvImage cv_image;
+void eventsCallback(const dvs_msgs::EventArray::ConstPtr& msg)
+{
+  if (image_pub_.getNumSubscribers() > 0)
+  {
+    cv_bridge::CvImage cv_image;
 
-  cv_image.encoding = "bgr8";
-  cv_image.image = cv::Mat(128, 128, CV_8UC3);
-  cv_image.image = cv::Scalar(128, 128, 128);
+    if (false)
+    {
+      cv_image.encoding = "bgr8";
+      cv_image.image = cv::Mat(128, 128, CV_8UC3);
+      cv_image.image = cv::Scalar(128, 128, 128);
 
-  for (int i=0; i<msg->events.size(); ++i) {
-    int x = msg->events[i].x;
-    int y = msg->events[i].y;
+      for (int i = 0; i < msg->events.size(); ++i)
+      {
+        int x = msg->events[i].x;
+        int y = msg->events[i].y;
 
-    if (x < 0 || x > 127 || y < 0 || y > 127) {
-      ROS_WARN("Pixel location out of range!");
+        cv_image.image.at<cv::Vec3b>(y, x) = (
+            msg->events[i].polarity == true ? cv::Vec3b(255, 0, 0) : cv::Vec3b(0, 0, 255));
+      }
+    }
+    else
+    {
+      cv_image.encoding = "mono8";
+      cv_image.image = cv::Mat(128, 128, CV_8U);
+      cv_image.image = cv::Scalar(128);
+
+      cv::Mat on_events = cv::Mat(128, 128, CV_8U);
+      on_events = cv::Scalar(0);
+
+      cv::Mat off_events = cv::Mat(128, 128, CV_8U);
+      off_events = cv::Scalar(0);
+
+      // count events per pixels with polarity
+      for (int i = 0; i < msg->events.size(); ++i)
+      {
+        int x = msg->events[i].x;
+        int y = msg->events[i].y;
+
+        if (msg->events[i].polarity == 1)
+          on_events.at<uint8_t>(y, x)++;
+        else
+          off_events.at<uint8_t>(y, x)++;
+      }
+
+      // scale image
+      cv::normalize(on_events, on_events, 0, 128, cv::NORM_MINMAX, CV_8UC1);
+      cv::normalize(off_events, off_events, 0, 127, cv::NORM_MINMAX, CV_8UC1);
+
+      cv_image.image += on_events;
+      cv_image.image -= off_events;
     }
 
-    cv_image.image.at<cv::Vec3b>(y,x) = (msg->events[i].polarity == true ? cv::Vec3b(255, 0, 0) : cv::Vec3b(0, 0, 255));
+    image_pub_.publish(cv_image.toImageMsg());
   }
-
-  image_pub_.publish(cv_image.toImageMsg());
 }
 
 int main(int argc, char* argv[])
