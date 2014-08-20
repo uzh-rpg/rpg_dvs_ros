bbcc3ef80bb7e08a2043a00f5f2a7d4fcd6f3b5c
Elias Mueggler <elias.mueggler@gmail.com>
2014-08-20 16:01:00 +0200

    removed visp dependency and added running average computation

--- dvs_camera_rig_calibration/src/calibration.cpp
+++ dvs_camera_rig_calibration/src/calibration.cpp
@@ -1,6 +1,6 @@
 #include "ros/ros.h"
 
-#include <visp_hand2eye_calibration/compute_effector_camera_quick.h>
+//#include <visp_hand2eye_calibration/compute_effector_camera_quick.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Transform.h>
 
@@ -12,24 +12,12 @@ ros::ServiceClient callHandEyeCalibrationSrv;
 
 ros::Publisher poseDifferencePub;
 
-visp_hand2eye_calibration::compute_effector_camera_quick calibSrv;
+//visp_hand2eye_calibration::compute_effector_camera_quick calibSrv;
 
 double max_time_difference = 0.1;
 
-geometry_msgs::Transform poseToTrafo(geometry_msgs::Pose p)
-{
-  geometry_msgs::Transform t;
-  t.translation.x = p.position.x;
-  t.translation.y = p.position.y;
-  t.translation.z = p.position.z;
-
-  t.rotation.x = p.orientation.x;
-  t.rotation.y = p.orientation.y;
-  t.rotation.z = p.orientation.z;
-  t.rotation.w = p.orientation.w;
-
-  return t;
-}
+Eigen::Vector3d relative_sum = Eigen::Vector3d::Zero();
+int num_measurements = 0;
 
 geometry_msgs::PoseStamped lastHandPose;
 void handCallback(const geometry_msgs::PoseStamped::ConstPtr& msg)
@@ -41,42 +29,54 @@ void eyeCallback(const geometry_msgs::PoseStamped::ConstPtr& eyePose)
 {
   if (eyePose->header.stamp - lastHandPose.header.stamp < ros::Duration(max_time_difference))
   {
-    calibSrv.request.world_effector.transforms.push_back(poseToTrafo(lastHandPose.pose));
-    calibSrv.request.camera_object.transforms.push_back(poseToTrafo(eyePose->pose));
-
-    // compute relative pose between hand and eye
+    // convert to eigen
     Eigen::Affine3d handPoseEigen, eyePoseEigen;
     tf::poseMsgToEigen(lastHandPose.pose, handPoseEigen);
     tf::poseMsgToEigen(eyePose->pose, eyePoseEigen);
 
+    // invert poses
+    geometry_msgs::Transform handTransform, eyeTransform;
+    tf::transformEigenToMsg(handPoseEigen.inverse(), handTransform);
+    tf::transformEigenToMsg(eyePoseEigen.inverse(), eyeTransform);
+//    calibSrv.request.world_effector.transforms.push_back(handTransform);
+//    calibSrv.request.camera_object.transforms.push_back(eyeTransform);
+
+    // compute relative pose between hand and eye
     Eigen::Affine3d relativePoseEigen = handPoseEigen * eyePoseEigen.inverse();
 
     geometry_msgs::PoseStamped relative_pose;
     tf::poseEigenToMsg(relativePoseEigen, relative_pose.pose);
-
     poseDifferencePub.publish(relative_pose);
+//    ROS_INFO("%f %f %f %f", relative_pose.pose.position.x, relative_pose.pose.position.y, relative_pose.pose.position.z, relative_pose.pose.orientation.w);
 
+    // running average
+    relative_sum += relativePoseEigen.translation();
+    num_measurements++;
 
-    ROS_INFO("Added another measurement.");
+    ROS_INFO("Running average (n=%d): %2.6f %2.6f %2.6f [m]", num_measurements,
+             relative_sum.x()/num_measurements, relative_sum.y()/num_measurements,
+             relative_sum.z()/num_measurements);
 
-    if (calibSrv.request.camera_object.transforms.size() > 20)
-    {
-      if (callHandEyeCalibrationSrv.call(calibSrv))
-      {
-        ROS_INFO("Trafo is %2.3f %2.3f %2.3f %2.3f %2.3f %2.3f %2.3f",
-                 calibSrv.response.effector_camera.translation.x,
-                 calibSrv.response.effector_camera.translation.y,
-                 calibSrv.response.effector_camera.translation.z,
-                 calibSrv.response.effector_camera.rotation.x,
-                 calibSrv.response.effector_camera.rotation.y,
-                 calibSrv.response.effector_camera.rotation.z,
-                 calibSrv.response.effector_camera.rotation.w);
-      }
-      else
-      {
-        ROS_ERROR("Error calling hand-eye calibration service.");
-      }
-    }
+//    ROS_INFO("Added another measurement.");
+//
+//    if (calibSrv.request.camera_object.transforms.size() > 20)
+//    {
+//      if (callHandEyeCalibrationSrv.call(calibSrv))
+//      {
+//        ROS_INFO("Trafo is %2.3f %2.3f %2.3f %2.3f %2.3f %2.3f %2.3f",
+//                 calibSrv.response.effector_camera.translation.x,
+//                 calibSrv.response.effector_camera.translation.y,
+//                 calibSrv.response.effector_camera.translation.z,
+//                 calibSrv.response.effector_camera.rotation.x,
+//                 calibSrv.response.effector_camera.rotation.y,
+//                 calibSrv.response.effector_camera.rotation.z,
+//                 calibSrv.response.effector_camera.rotation.w);
+//      }
+//      else
+//      {
+//        ROS_ERROR("Error calling hand-eye calibration service.");
+//      }
+//    }
   }
 }
 
@@ -95,7 +95,7 @@ int main(int argc, char* argv[])
 
   poseDifferencePub = nh.advertise<geometry_msgs::PoseStamped>("relative_pose", 1);
 
-  callHandEyeCalibrationSrv = nh.serviceClient<visp_hand2eye_calibration::compute_effector_camera_quick>("compute_effector_camera_quick");
+//  callHandEyeCalibrationSrv = nh.serviceClient<visp_hand2eye_calibration::compute_effector_camera_quick>("compute_effector_camera_quick");
 
   ros::spin();
 
