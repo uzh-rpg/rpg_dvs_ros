bbd942058176e8452720233173bfb522896f3a30
Elias Mueggler <elias.mueggler@gmail.com>
2014-11-27 13:02:13 +0100

    added license headers

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ The RPG ROS DVS package has been tested under ROS Indigo and Ubuntu 14.04.
 
 This is research code, expect that it changes often and any fitness for a particular purpose is disclaimed.
 
-The source code is released under an **GNU General Public License (GPL)**.
+The source code is released under a **GNU General Public License (GPL)**.
 
 
 # Package Overview
--- dvs_renderer/src/renderer.cpp
+++ dvs_renderer/src/renderer.cpp
@@ -1,4 +1,19 @@
-#include "ros/ros.h"
+// This file is part of DVS-ROS - the RPG DVS ROS Package
+//
+// DVS-ROS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// DVS-ROS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with DVS-ROS.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <ros/ros.h>
 
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
