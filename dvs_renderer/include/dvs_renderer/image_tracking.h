3b47e36fc2109279b0034fee0199961b9b4f7685
Beat Küng <beat-kueng@gmx.net>
2016-03-09 14:17:39 +0100

    renderer: move ImageTracking class into separate file & add edges overlay output

--- dvs_renderer/include/dvs_renderer/image_tracking.h
+++ dvs_renderer/include/dvs_renderer/image_tracking.h
@@ -13,21 +13,17 @@
 // You should have received a copy of the GNU General Public License
 // along with DVS-ROS.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef DVS_RENDERER_H_
-#define DVS_RENDERER_H_
+#pragma once
 
 #include <ros/ros.h>
 
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/CameraInfo.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-
-#include "image_tracking.h"
 
 #include <dvs_msgs/Event.h>
 #include <dvs_msgs/EventArray.h>
@@ -35,49 +31,38 @@
 namespace dvs_renderer
 {
 
-class Renderer {
+/**
+ * @class ImageTracking
+ * Track frames over 1 sec, then project all events during this time into
+ * the latest frame & output the accumulated events per pixel.
+ * This requires the scene to be planar, and for best results, the camera motion
+ * is perpendicular to the plane's surface normal.
+ */
+class ImageTracking {
 public:
-  Renderer(ros::NodeHandle & nh, ros::NodeHandle nh_private);
-  virtual ~Renderer();
-
-private:
-  ros::NodeHandle nh_;
-
-  void cameraInfoCallback(const sensor_msgs::CameraInfo::ConstPtr& msg);
+  ImageTracking(ros::NodeHandle & nh);
   void eventsCallback(const dvs_msgs::EventArray::ConstPtr& msg);
   void imageCallback(const sensor_msgs::Image::ConstPtr& msg);
+private:
 
-  void publishStats();
+  void render();
+  void reset();
 
-  bool got_camera_info_;
-  cv::Mat camera_matrix_, dist_coeffs_;
-
-  ros::Subscriber event_sub_;
-  ros::Subscriber camera_info_sub_;
-
-  image_transport::Publisher image_pub_;
-  image_transport::Publisher undistorted_image_pub_;
-
-  image_transport::Subscriber image_sub_;
-  cv::Mat last_image_;
-  bool used_last_image_;
-
-  struct EventStats {
-    ros::Publisher events_mean_[2]; /**< event stats output */
-    int events_counter_[2]; /**< event counters for on/off events */
-    double events_mean_lasttime_;
-    double dt;
+  struct ImgData {
+    cv::Mat img;
+    ros::Time t;
+    std::vector<cv::Point2f> points;
+    cv::Mat homography; //homography from previous image to this
+    cv::Mat homography_accumulated; //homography from previous image to the second last
+    cv::Point2f translation; //mean translation, previous to this
   };
-  EventStats event_stats_[2]; /**< event statistics for 1 and 5 sec */
-
-  enum DisplayMethod
-  {
-    GRAYSCALE, RED_BLUE
-  } display_method_;
-
-  ImageTracking image_tracking_;
+  std::vector<ImgData> images_;
+  std::vector<dvs_msgs::Event> events_;
+  double start_time_;
+  cv::Mat edges_;
+  image_transport::Publisher image_pub_;
+  image_transport::Publisher image_pub_events_edges_;
 };
 
-} // namespace
 
-#endif // DVS_RENDERER_H_
+} // namespace
