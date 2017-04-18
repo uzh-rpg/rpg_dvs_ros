81d1c57200ee4756c54b87c29444b6d55c953559
Elias Mueggler <elias.mueggler@gmail.com>
2017-04-18 15:15:15 +0200

    u

--- libcaer_catkin/cmake/libcaer-extras.cmake
+++ libcaer_catkin/cmake/libcaer-extras.cmake
@@ -1,3 +1,3 @@
-# This overrides the dependency tracker with the ceres library file.
+# This overrides the dependency tracker with the libcaer library file.
 set(@PROJECT_NAME@_LIBRARIES
     @CATKIN_DEVEL_PREFIX@/lib/libcaer${CMAKE_SHARED_LIBRARY_SUFFIX})
