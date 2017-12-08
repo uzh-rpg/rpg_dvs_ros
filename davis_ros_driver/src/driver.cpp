fce32ec6688710518c51d5b2d8567b3ecd59f6d3
Henri Rebecq <h.rebecq@gmail.com>
2017-12-08 09:43:58 +0100

    guillermogb on @54

--- davis_ros_driver/src/driver.cpp
+++ davis_ros_driver/src/driver.cpp
@@ -325,21 +325,23 @@ void DavisRosDriver::changeDvsParameters()
                 if (current_config_.imu_acc_scale >= 0 && current_config_.imu_acc_scale <= 3)
                     caerDeviceConfigSet(davis_handle_, DAVIS_CONFIG_IMU, DAVIS_CONFIG_IMU_ACCEL_FULL_SCALE, current_config_.imu_acc_scale);
 
-                if (current_config_.imu_low_pass_filter > 0 && current_config_.imu_low_pass_filter <= 6)
+                if (current_config_.imu_low_pass_filter >= 0 && current_config_.imu_low_pass_filter <= 6)
                 {
                     caerDeviceConfigSet(davis_handle_, DAVIS_CONFIG_IMU, DAVIS_CONFIG_IMU_DIGITAL_LOW_PASS_FILTER, current_config_.imu_low_pass_filter);
 
-                    // When the low pass filter is enabled, the gyroscope output rate is set to 1 kHz,
-                    // so we should not use the sample rate divider, in order to keep the IMU output rate to 1 kHz.
-                    caerDeviceConfigSet(davis_handle_, DAVIS_CONFIG_IMU, DAVIS_CONFIG_IMU_SAMPLE_RATE_DIVIDER, 0);
-                }
-                else if (current_config_.imu_low_pass_filter == 0)
-                {
-                    // If we disable the low pass filter, then the output frequency of IMU events
-                    // will be raised to 8KHz. To keep it to 1 kHz, we use the sample rate divider
-                    // (setting its value to 7 divides the frequency by 8).
-                    caerDeviceConfigSet(davis_handle_, DAVIS_CONFIG_IMU, DAVIS_CONFIG_IMU_DIGITAL_LOW_PASS_FILTER, current_config_.imu_low_pass_filter);
-                    caerDeviceConfigSet(davis_handle_, DAVIS_CONFIG_IMU, DAVIS_CONFIG_IMU_SAMPLE_RATE_DIVIDER, 7);
+                    if(current_config_.imu_low_pass_filter == 0)
+                    {
+                      // When the low pass filter is disabled, the output frequency of IMU events
+                      // is raised to 8KHz. To keep it to 1 kHz, we use the sample rate divider
+                      // (setting its value to 7 divides the frequency by 8).
+                      caerDeviceConfigSet(davis_handle_, DAVIS_CONFIG_IMU, DAVIS_CONFIG_IMU_SAMPLE_RATE_DIVIDER, 7);
+                    }
+                    else
+                    {
+                      // When the low pass filter is enabled, the gyroscope output rate is set to 1 kHz,
+                      // so we should not use the sample rate divider, in order to keep the IMU output rate to 1 kHz.
+                      caerDeviceConfigSet(davis_handle_, DAVIS_CONFIG_IMU, DAVIS_CONFIG_IMU_SAMPLE_RATE_DIVIDER, 0);
+                    }
                 }
             }
             /*
