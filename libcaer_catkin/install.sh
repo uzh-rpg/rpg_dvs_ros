51f97d6b7a295e244fa9a506801e289fc84599c8
Luca Longinotti <luca.longinotti@inivation.com>
2019-06-03 16:57:22 +0200

    Update iniLabs -> iniVation. Use newest libcaer 3.2.0, requires new firmware/logic.

--- libcaer_catkin/install.sh
+++ libcaer_catkin/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Copying udev rule (needs root privileges)."
-sudo cp 65-inilabs.rules /etc/udev/rules.d/
+sudo cp 65-inivation.rules /etc/udev/rules.d/
 
 echo "Reloading udev rules."
 sudo udevadm control --reload-rules
